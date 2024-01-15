7558aa591ced1bed1ef44164ecf02437900244f3
BeLieVer031 <sandyrajak031@gmail.com>
2024-01-15 17:14:46 +0530

    song fetching from appwrite done

--- pages/genre-songs/genre-song.js
+++ pages/genre-songs/genre-song.js
@@ -1,3 +1,5 @@
+import { Query, databases, storage } from "../../appwrite/config";
+import { BUCKET_ID, DATABASE_ID, SONG_COLLECTION_ID } from "../../utils/sceret";
 const activeGenre = document.querySelector("#curr-genre")
 const genreSongCont = document.querySelector("#genre-song-cont")
 let audioElement = document.querySelector("audio")
@@ -7,7 +9,7 @@ const section = document.querySelector("section")
 let totalDuration = document.querySelector("#t-duration")
 let cTime = document.querySelector("#c-time")
 const myBar = document.querySelector(".myBar")
-
+const GENRE_NAME = window.location.href.split("?")[1].split("=")[1]
 let calculationCount = 1;
 let intervalInSeconds = 3;
 let checkWhenSongPlayHappen = "first";
@@ -16,48 +18,51 @@ let prevSong = null;
 let currTrack = null;
 let index = 0;
 let isLeftDown = false;
+let songArr = [];
+// const songArr = [
+//     {
+//         _id: 1,
+//         songName: "RamSiyaRam",
+//         src: "../../public/RamSiyaRam.webm",
+//         artist: "Latish"
+//     },
+//     {
+//         _id: 2,
+//         songName: "maan meri jaan",
+//         src: "../../public/maanmerijaan.mp3",
+//         artist: "Latish"
+//     },
+//     {
+//         _id: 3,
+//         songName: "sanam re",
+//         src: "../../public/sanamre.webm",
+//         artist: "sachin"
+//     },
+//     {
+//         _id: 4,
+//         songName: "phiraurkya",
+//         src: "../../public/phiraurkya.webm",
+//         artist: "sachin"
 
-const songArr = [
-    {
-        _id: 1,
-        songName: "RamSiyaRam",
-        src: "../../public/RamSiyaRam.webm",
-        artist: "Latish"
-    },
-    {
-        _id: 2,
-        songName: "maan meri jaan",
-        src: "../../public/maanmerijaan.mp3",
-        artist: "Latish"
-    },
-    {
-        _id: 3,
-        songName: "sanam re",
-        src: "../../public/sanamre.webm",
-        artist: "sachin"
-    },
-    {
-        _id: 4,
-        songName: "phiraurkya",
-        src: "../../public/phiraurkya.webm",
-        artist: "sachin"
+//     },
+//     {
+//         _id: 5,
+//         songName: "Majha raja tu sona",
+//         src: "../../public/Majharajatusona.webm",
+//         artist: "Sandeep"
 
-    },
-    {
-        _id: 5,
-        songName: "Majha raja tu sona",
-        src: "../../public/Majharajatusona.webm",
-        artist: "Sandeep"
+//     },
 
-    },
+// ]
 
-]
+
 
 const fetchSongByGenreName = () => {
+    console.log(songArr);
     songArr.forEach((element, idx) => {
         const songBox = document.createElement("div");
         songBox.classList.add("genre-song-box")
-        const html = `<div class="song-title">${element.songName}</div>
+        const html = `<div class="song-title">${element.title}</div>
             <div class="song-artist">${element.artist}</div>`
         songBox.innerHTML = html
         genreSongCont.appendChild(songBox)
@@ -76,16 +81,34 @@ const fetchSongByGenreName = () => {
     })
 }
 
-fetchSongByGenreName();
+async function fetchSongFromDB() {
+    try {
+        const res = await databases.listDocuments(DATABASE_ID, SONG_COLLECTION_ID,
+            [
+                Query.limit(10),
+                Query.equal("genre", [GENRE_NAME])
+            ])
+        songArr = res.documents
+        fetchSongByGenreName();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchSongFromDB();
+
 
 async function handlePlayAndPause(index, isEnded = null) {
     if (isPlay && isEnded === null) {
         playAndPause.innerHTML = "play_arrow"
         audioElement.pause();
     } else {
-        
+
         if (index >= 0 && prevSong !== index) {
-            audioElement.src = songArr[index].src
+            // audioElement.src = songArr[index].src
+            audioElement.src = storage.getFileDownload(BUCKET_ID, songArr[index].songid)
+
         }
 
         await audioElement.play()
@@ -151,7 +174,7 @@ function handleResetMyBar() {
 }
 
 progressBar.addEventListener("click", (event) => {
-    seek(event,true)
+    seek(event, true)
 })
 
 progressBar.addEventListener("mousedown", (e) => {
@@ -171,6 +194,7 @@ progressBar.addEventListener("mouseup", (event) => {
     isLeftDown = false;
     seek(event, true)
 })
+
 async function seek(event, isMouseUp = false) {
     var percent = (event.offsetX / progressBar.offsetWidth);
     myBar.style.width = `${percent * 100}%`
